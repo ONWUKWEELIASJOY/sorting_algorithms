44aef5f96e30aad41b46b7b4a30a88015bac2c54
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-04-25 08:19:59 -0700

    first sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,47 @@
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
-  * bubble_sort - funcyion for sorting the bubble
-  * @array: array to sort.
-  * @size: size of array
-  * Return: 0
-  */
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+
+/*bubble_sort, @array, @size*/
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t a = 0, b = 0;
-	int c = 0;
+	size_t i = 0, j = 0;
+	int aux = 0;
 
 	if (array == NULL || size == 0)
-		return 0;
-
-	for (a < size - 1; a++)
+		return;
+	for (; i < size - 1; i++)
 	{
-		for (b = 0; b < size - a - 1; b++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[b] > array[b + 1])
+			if (array[j] > array[j + 1])
 			{
-				array[b + 1] = array[b];
-				c = array[b + 1];
-				array[b] = c;
+				aux = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = aux;
 				print_array(array, size);
 			}
 		}
